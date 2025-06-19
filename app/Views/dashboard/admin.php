c77f9135cfdb862cc259201a66ba3d34945222a5
Anto Widianto <kentowidianto@gmail.com>
2025-06-19 11:05:08 +0700

    push

--- app/Views/dashboard/admin.php
+++ app/Views/dashboard/admin.php
@@ -1,5 +1,5 @@
 <?= $this->extend('layouts/main') ?>
 <?= $this->section('content') ?>
   <h2 class="text-2xl font-semibold mb-4">Admin Dashboard</h2>
-  <p>You have admin access.</p>
+  <p>You have admin access for now</p>
 <?= $this->endSection() ?>
